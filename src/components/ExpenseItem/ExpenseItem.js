9e5753078f24731a4d8df1e124265e4c7c2373e7
janiceh94 <haesung94@gmail.com>
2022-12-21 23:02:28 -0800

    created data to display

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,15 +1,14 @@
 import "./ExpenseItem.css";
 
-export default function ExpenseItem(){
+export default function ExpenseItem(props) {
+
     return (
-        <div className="expense-item">
-            <div>March 28th 2021</div>
-            <div  className="expense-item-description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item-price">
-                    $294.67
-                </div>
-            </div>
+      <div className='expense-item'>
+        <div className='expense-item-description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item-price'>${props.amount}</div>
         </div>
-    )
-}
+      </div>
+    );
+  }
+  
