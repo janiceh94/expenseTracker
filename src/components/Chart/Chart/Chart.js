d76d5e9f636bd122bb6c37d95855e11cf257c62d
janiceh94 <haesung94@gmail.com>
2022-12-29 22:26:22 -0800

    created chart bar that responds to current year

--- src/components/Chart/Chart/Chart.js
+++ src/components/Chart/Chart/Chart.js
@@ -2,6 +2,14 @@ import "./Chart.css";
 import ChartBar from "../ChartBar/ChartBar";
 
 export default function Chart(props){
+
+    const dataPointValues = props.dataPoints.map(dataPoint => {
+        return (
+            dataPoint.value
+        )
+    });
+    const totalMax = Math.max(...dataPointValues)
+
     return (
         <div className="chart">
             {props.dataPoints.map(dataPoint => {
@@ -9,6 +17,7 @@ export default function Chart(props){
                     <ChartBar 
                         key={dataPoint.id}
                         value={dataPoint.value}
+                        maxValue={totalMax}
                         label={dataPoint.label}
                     />
                 )
--- src/components/Expense/Expenses/Expenses.js
+++ src/components/Expense/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import {useState} from "react";
 import Card from "../../UI/Card/Card";
 import ExpensesFilter from "../ExpenseFilter/ExpenseFilter";
 import ExpenseList from "../ExpenseList/ExpenseList";
+import ExpensesChart from "../ExpensesChart/ExpensesChart";
 
 export default function Expenses(props){
 
@@ -21,6 +22,7 @@ export default function Expenses(props){
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={handleFilter} />
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpenseList items={filteredExpenses} />
         </Card>
     )
