7ee6b1c9ed0bfd2b9194e30a1ea5e1de66106822
janiceh94 <haesung94@gmail.com>
2022-12-29 21:46:33 -0800

    wrap card in expenseItem with a li

--- src/components/Expense/ExpenseItem/ExpenseItem.js
+++ src/components/Expense/ExpenseItem/ExpenseItem.js
@@ -5,13 +5,15 @@ import Card from "../../UI/Card/Card";
 export default function ExpenseItem(props) {
 
     return (
-      <Card className='expense-item'>
-        <ExpenseDate date={props.date}/>
-        <div className='expense-item-description'>
-          <h2>{props.title}</h2>
-          <div className='expense-item-price'>${props.amount}</div>
-        </div>
-      </Card>
+      <li>
+        <Card className='expense-item'>
+          <ExpenseDate date={props.date}/>
+          <div className='expense-item-description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item-price'>${props.amount}</div>
+          </div>
+        </Card>
+      </li>
     );
   }
   
